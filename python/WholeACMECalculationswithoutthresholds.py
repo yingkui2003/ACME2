08bbb3e6900ad65b7b5beaa54de100345975740b
Yingkui Li <liyingkui2003@gmail.com>
2024-10-03 20:23:12 -0400

    Add files via upload

--- python/WholeACMECalculationswithoutthresholds.py
+++ python/WholeACMECalculationswithoutthresholds.py
@@ -773,18 +773,23 @@ def connect_line_sections(line):
     
     return line
 
+
 def define_circle_center(p1, p2, p3):
     """
     Returns the center and radius of the circle passing the given 3 points.
-    In case the 3 points form a line, returns (None, infinity).
+    In case the 3 points form a line, returns the middle point.
     """
     temp = p2[0] * p2[0] + p2[1] * p2[1]
     bc = (p1[0] * p1[0] + p1[1] * p1[1] - temp) / 2
     cd = (temp - p3[0] * p3[0] - p3[1] * p3[1]) / 2
     det = (p1[0] - p2[0]) * (p2[1] - p3[1]) - (p2[0] - p3[0]) * (p1[1] - p2[1])
-    
+
     if abs(det) < 1.0e-6:
-        return None
+        #arcpy.AddMessage("The three points are along the same line")
+        cx = (p1[0] + p2[0]) / 2
+        cy = (p1[1] + p2[1]) / 2
+        return (cx, cy)
+        #return None
     
     # Center of circle
     cx = (bc*(p2[1] - p3[1]) - cd*(p1[1] - p2[1])) / det
